e3ea8f6e1a0c6bbf384cea09af87b7b5980f6827
vicentewncosta <vicentecoosta55@gmail.com>
2023-04-06 15:35:01 -0300

    adicionando forEach

--- index.js
+++ index.js
@@ -4,17 +4,24 @@ const app = express();
 //dizendo para o express usar o ejs como view engine
 app.set('view engine', 'ejs');
 
-
 app.get("/:nome/:lang", (req, res) => {
     var nome = req.params.nome;
     var lang = req.params.lang;
-    var exibirMsg = true;
+    var exibirMsg = false;
+
+    var produtos = [
+        {nome: "Doritos",preco: 3.14},
+        {nome: "coca-cola",preco:5},
+        {nome: "Leite", preco: 7.50},
+    ] 
+
     res.render("index",{
         nome: nome, 
         lang: lang,
         empresa: "vicentetesteempresa",
         inscritos: 120000,
-        msg: exibirMsg
+        msg: exibirMsg,
+        produtos: produtos
     });
 });
 
