ff906af5d05446a87e8a9516413c2072f707794f
vicentewncosta <vicentecoosta55@gmail.com>
2023-04-16 16:01:56 -0300

    adicionando body parser

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express(); 
+const bodyParser = require("body-parser");
 
 //dizendo para o express usar o ejs como view engine
 app.set('view engine', 'ejs');
@@ -7,6 +8,11 @@ app.set('view engine', 'ejs');
 //arquivos estáticos
 app.use(express.static('public'));
 
+//importando body-parser
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json());
+
+//rotas
 app.get("/", (req, res) => {
     res.render("index");
 });
@@ -14,5 +20,11 @@ app.get("/", (req, res) => {
 app.get("/perguntar", (req, res) => {
     res.render("perguntar");
 }) 
+
+app.post("/salvarpergunta", (req, res) => {
+    var titulo = req.body.titulo;
+    var descricao = req.body.descricao;
+    res.send("formulário recebido! titulo " + titulo + " " + " descricao " + descricao);
+});
 
 app.listen(8080, () => {console.log("APP RODANDO NA PORTA 8080!");});
